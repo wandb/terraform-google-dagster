8c579669e9b5963f22a41a09546d626d9b134e7d
Ross Nanopoulos <2287187+zythosec@users.noreply.github.com>
2022-07-12 12:05:59 -0400

    fix: Explicitly disable client certificate auth (deprecated by kubernetes) (#6)

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -19,6 +19,15 @@ resource "google_container_cluster" "default" {
 
   remove_default_node_pool = true
   initial_node_count       = 1
+
+  # Disable client certificate authentication, which reduces the attack surface 
+  # for the cluster by disabling this deprecated feature. It defaults to false, 
+  # but this will make it explicit and quiet some security tooling.
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
 }
 
 resource "google_container_node_pool" "default" {
