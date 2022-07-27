ffc561cd6f86229e5f1fddadb285ab1e443455bd
moonshot-s <105249796+moonshot-s@users.noreply.github.com>
2022-07-27 13:05:08 -0700

    refactor: Updates to exclusively create a VPC network (#7)
    * Updates to exclusively create a VPC network
    
    * refactor: updates to exclusively create a VPC network
    
    * Apply linting

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -2,3 +2,12 @@ variable "namespace" {
   description = "The application name used as a prefix for all resources created."
   type        = string
 }
+variable "project" {
+  description = "Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Google region"
+  type        = string
+}
