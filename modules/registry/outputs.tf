d01fcd8e034728b70ca8900c8e6fd99966460bf9
Chris Histe <chrishiste@gmail.com>
2023-08-18 11:27:27 -0400

    feat: Add two registry outputs (#13)
    * feat: Add two registry outputs
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/registry/outputs.tf
+++ modules/registry/outputs.tf
@@ -3,6 +3,16 @@ output "registry" {
   value       = google_artifact_registry_repository.default
 }
 
+output "registry_name" {
+  description = "Name of provisioned Artifact Registry"
+  value       = google_artifact_registry_repository.default.name
+}
+
+output "registry_location" {
+  description = "Location of provisioned Artifact Registry"
+  value       = google_artifact_registry_repository.default.location
+}
+
 output "registry_image_path" {
   description = "Path to registry repository images (ex: example_repo-docker.pkg.dev/my-project/dagster-images)"
   value       = "${google_artifact_registry_repository.default.location}-docker.pkg.dev/${google_artifact_registry_repository.default.project}/${google_artifact_registry_repository.default.repository_id}"
